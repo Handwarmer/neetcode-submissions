978edc90cd3b85cd3e2fb8328ed0befe478bfa26
Handwarmer <12636986+Handwarmer@users.noreply.github.com>
2026-09-05 22:04:25 -0700

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.java
@@ -0,0 +1,44 @@
+class Solution {
+    record Point(int r, int c) {}
+    List<List<String>> res = new ArrayList<>();
+    Set<Integer> dia1 = new HashSet<>();
+    Set<Integer> dia2 = new HashSet<>();
+    Set<Integer> cols = new HashSet<>();
+    int[][] dir = new int[][]{{1,1},{-1,1},{1,-1},{-1,-1}};
+    int n;
+    public List<List<String>> solveNQueens(int n) {
+        this.n = n;
+        backtrack(0, new ArrayList<>());
+        return res;
+    }
+    void backtrack(int r, List<String> cur) {
+        if (cur.size() == n) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for (int c = 0; c < n; c ++) {
+            if (cols.contains(c)) continue;
+            if (dia1.contains(r + c)) continue;
+            if (dia2.contains(r - c)) continue;
+            cur.add(buildStr(c));
+            cols.add(c);
+            dia1.add(r+c);
+            dia2.add(r-c);
+            backtrack(r+1, cur);
+            dia1.remove(r+c);
+            dia2.remove(r-c);
+            cols.remove(c);
+            cur.remove(cur.size()-1);
+        }
+    }
+    private String buildStr(int c) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i ++) {
+            if (i == c)
+                sb.append('Q');
+            else
+                sb.append('.');
+        }
+        return sb.toString();
+    }
+}
