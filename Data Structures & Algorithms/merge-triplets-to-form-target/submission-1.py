ca6f5f6f4de011acf7ca8536b95de80e136509e6
Handwarmer <12636986+Handwarmer@users.noreply.github.com>
2026-09-17 16:46:40 -0700

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        
+        has = [False, False, False]
+        for t in triplets:
+            canUse = True
+            curHas = [False, False, False]
+            for i in range(0, 3):
+                if t[i] > target[i]:
+                    canUse = False
+                    break
+                elif t[i] == target[i]:
+                    curHas[i] = True
+            if canUse:
+                for i in range(0, 3):
+                    has[i] = has[i] or curHas[i]
+        
+        return all(has)
